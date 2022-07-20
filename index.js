a338811777f9be8772612435711b5fd4ce8b74bf
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-20 14:10:03 -0400

    commit tree 5d2388fa9271605b8a1ac6443e0446061aa1ed40

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil( Math.random() * 10 );
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 1, 5, 7, 8, 15, 20 );
